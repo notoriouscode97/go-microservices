0dc1abbacccba9ed7a885687a79ce1a650bd294a
Dusan Dimic <dusan.dimic@vegait.rs>
2024-09-25 09:45:18 +0200

    Adds gracefull shutdown, newServeMux & timeouts

--- main.go
+++ main.go
@@ -1,30 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"context"
+	"github.com/notoriouscode97/handlers"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
-		d, err := io.ReadAll(r.Body)
+	l := log.New(os.Stdout, "product-api", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
 
 		if err != nil {
-			http.Error(w, "Oops", http.StatusBadRequest)
-			return
+			l.Fatal(err)
 		}
+	}()
 
-		fmt.Fprintf(w, "Hello %s", d)
-	})
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
 
-	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye World")
-	})
+	sig := <-sigChan
+	l.Println("Received terminate, gracefully shutting down", sig)
 
-	err := http.ListenAndServe(":9090", nil)
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+
+	defer cancel()
+
+	err := s.Shutdown(tc)
 
 	if err != nil {
 		log.Fatal(err)
